287dec2278e2da016aa4463e879b2c53aaf8f598
TAnjum-01 <87169399+TAnjum-01@users.noreply.github.com>
2023-04-04 21:41:49 +0600

    change in templates

--- guideBard/settings.py
+++ guideBard/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-7)gwwhpz8cd5+o061@&aywqy**2($%w9xxxl1ox+pup9=qyu9d
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -46,6 +44,7 @@ INSTALLED_APPS = [
     'tour_guides',
     'tourist_spots',
     'tourists',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -78,7 +77,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'guideBard.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -88,7 +86,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -108,7 +105,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -120,7 +116,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
@@ -130,3 +125,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- guideBardApp/views.py
+++ guideBardApp/views.py
@@ -1,29 +1,64 @@
+from django.contrib import messages
 from django.shortcuts import render, HttpResponse, redirect, HttpResponseRedirect
-from django.contrib.auth.forms import forms, authenticate
+from django.contrib.auth.forms import forms, authenticate, AuthenticationForm
 from django.forms.formsets import formset_factory
-from django.contrib.auth import settings
+from django.contrib.auth import settings, logout
 from django.contrib.auth.models import User
+
 
 def homePage(request):
     return render(request, 'home.html')
 
+
 def signUp(request):
     return render(request, 'sign-up.html')
+
 
 def login(request):
     return render(request, 'login.html')
 
+
 def tourGuide(request):
     return render(request, 'tour-guide.html')
+
 
 def reviews(request):
     return render(request, 'review.html')
 
+
 def profile(request):
     return render(request, 'profile.html')
+
 
 def contacts(request):
     return render(request, 'contacts.html')
 
+
 def payment(request):
     return render(request, 'home.html')
+
+
+def login_request(request):
+    global username, password
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.info(request, f"You are now logged in as {username}.")
+            return redirect('/')
+        else:
+            messages.error(request, "Invalid username or password.")
+    else:
+        messages.error(request, "Invalid username or password.")
+        form = AuthenticationForm()
+        return render(request=request, template_name="login.html", context={"login_form": form})
+
+
+def logout_request(request):
+    logout(request)
+    messages.info(request, "You have successfully logged out.")
+    return redirect("homepage")
