9cfcb702ff091f03fd6aa89d99d090516e6451d6
Ahmad Bin  Karim <114375529+ahmad-shakib11@users.noreply.github.com>
2023-04-04 23:56:30 +0600

    Update forms.py

---
+++ guideBardApp/forms.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+
+class UserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields =[
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+            'password1',
+            'password2',
+        ]
