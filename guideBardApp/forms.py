3579d80afb8baf0cb4ef756384b46c54388c5419
DM-Hasibul-Islam <dmhasibulislam233@gmail.com>
2023-05-06 08:37:59 +0600

    models and form

--- guideBardApp/forms.py
+++ guideBardApp/forms.py
@@ -1,6 +1,14 @@
+from django.contrib.auth.forms import forms
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from guideBardApp.models import touristSpots
+from guideBardApp.models import bookings
+from guideBardApp.models import emergencyContacts
+from guideBardApp.models import payments
+from guideBardApp.models import reviews
+from guideBardApp.models import areaTourGuide
 
-#user creation form
+
 class UserForm(UserCreationForm):
     class Meta:
         model = User
@@ -11,4 +19,64 @@ class UserForm(UserCreationForm):
             'email',
             'password1',
             'password2',
+        ]
+
+
+class touristSpot(forms.ModelForm):
+    class Meta:
+        model = touristSpots
+        fields = [
+            'spot_name',
+            'spot_details',
+        ]
+
+
+class bookingForm(forms.ModelForm):
+    class Meta:
+        model = bookings
+        fields = [
+            'bk_start_time',
+            'bk_end_time',
+            'bk_amount',
+        ]
+
+
+class emergencyContactsForm(forms.ModelForm):
+    class Meta:
+        model = emergencyContacts
+        fields = [
+            'station_name',
+            'em_contact',
+            # 'ts_id',
+        ]
+
+
+class paymentForm(forms.ModelForm):
+    class Meta:
+        model = payments
+        fields = [
+            'p_method',
+            'p_trxid',
+            'p_amount',
+            'p_date',
+        ]
+
+
+class reviewForm(forms.ModelForm):
+    class Meta:
+        model = reviews
+        fields = [
+            'tg_id',
+            't_id',
+            'r_review',
+            'r_star',
+        ]
+
+class areaTourGuideForm(forms.ModelForm):
+    class Meta:
+        model = areaTourGuide
+        fields = [
+            'at_area_name',
+            'tg_id',
+            'ts_id',
         ]
--- guideBardApp/models.py
+++ guideBardApp/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class touristSpots(models.Model):
+    spot_name = models.CharField(max_length=100)
+    spot_details = models.TextField()
+
+
+class bookings(models.Model):
+    bk_start_time = models.DateTimeField()
+    bk_end_time = models.DateTimeField()
+    bk_amount = models.FloatField()
+
+
+class emergencyContacts(models.Model):
+    station_name = models.CharField(max_length=100)
+    em_contact = models.IntegerField()
+
+class payments(models.Model):
+    p_method = models.CharField(max_length=50)
+    p_trxid = models.CharField(max_length=50)
+    p_amount = models.FloatField()
+    p_date = models.DateTimeField()
+
+
+class reviews(models.Model):
+    r_id = models.AutoField(primary_key=True)
+    r_review = models.TextField()
+    r_star = models.FloatField()
+
+class areaTourGuide(models.Model):
+    at_id = models.AutoField(primary_key=True)
+    at_area_name = models.CharField(max_length=50)
+    tg_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    ts_id = models.ForeignKey(touristSpots, on_delete=models.CASCADE)
